438e519517b470c2864ab2ed41ca75ff00461a44
ianandparmar <sameeranandparmar@gmail.com>
2022-08-10 23:47:17 +0530

    react_revison

--- src/Components/Practice6.js
+++ src/Components/Practice6.js
@@ -1,16 +1,44 @@
 import React from "react";
 
-class Practice6 extends React.Component {
-  constructor() {
-    super();
+// Event Handling using class Component
+
+// class Practice6 extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   testFun(){
+//     alert("test fun!");
+//   }
+
+//   render(){
+//        return(
+//               <div>
+//                      <h1>Event handling in react</h1>
+//                      <button onClick={this.testFun.bind()}>click me</button>
+//               </div>
+//        )
+//   }
+// }
+
+// Event Handling using Functional Component
+
+function Practice6() {
+
+  // function testFun(){
+  //   alert("test fun!");
+  // }
+
+  const testFun = () =>{
+    alert("test fun!");
   }
-  render(){
-       return(
-              <div>
-                     <h1>Event handling in react</h1>
-              </div>
-       )
-  }
+
+  return(
+    <div>
+      <h1>Event handling in react</h1>
+      <button onClick={testFun}>Click me</button>
+    </div>
+  )
 }
 
 export default Practice6;
